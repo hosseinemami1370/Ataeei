b35b35251cb1ffd2bd22f0ac8f94279a5bfa4acb
Mohammad Ataei <mammadataei@gmail.com>
2021-02-24 18:46:44 +0330

    add PostListItem component

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -1,13 +1,22 @@
 <template>
-  
+  <!-- <PropsItemList title="hello world"/> -->
+  <div v-if="isVisible">
+    <h2>{{ title }}</h2>
+    <a :href="link"></a>
+    <button @click="isVisible = false">Hide</button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  props: ['title', 'link'],
+  data () {
+    return {
+      isVisible: true
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
--- tests/unit/components/PostListItem.spec.js
+++ tests/unit/components/PostListItem.spec.js
@@ -1,11 +1,63 @@
-import PostListItem from '../../../components/PostListItem'
-import { render } from '@testing-library/vue'
+import PostListItem from '../../../src/components/PostListItem'
+import { fireEvent, render } from '@testing-library/vue'
 
-it('render a given title', () => {
-  render(PostListItem, {
-    props: {title: 'hello world!'}
+function getPostTitle () {
+  return document.getElementsByTagName('h2')[0]
+}
+
+function getPostLink () {
+  return document.getElementsByTagName('a')[0]
+}
+
+it('render a given title', async () => {
+  const { updateProps } = render(PostListItem, {
+    props: {
+      title: 'hello world!'
+    }
   })
 
-  expect(document.getElementByTagName('h2'))
-    .toHaveTextContent('hello world!') 
+  expect(getPostTitle())
+    .toHaveTextContent('hello world!')
+
+  await updateProps({
+    title: 'hello world agin!'
+  })
+
+  expect(getPostTitle())
+    .toHaveTextContent('hello world agin!')
+})
+
+test('renders the site link', async () => {
+  const { updateProps } = render(PostListItem, {
+    props: {
+      link: 'google.com'
+    }
+  })
+
+  expect(getPostLink())
+    .toHaveAttribute('href', 'google.com')
+
+  await updateProps({
+    link: 'facebook.com'
+  })
+
+  expect(getPostLink())
+    .toHaveAttribute('href', 'facebook.com')
+});
+
+test('user can hide item', async () => {
+  const { getByText, container } = render(PostListItem, {
+    props: {
+      title: 'Google',
+      link: 'google.com'
+    }
+  })
+
+  expect(container)
+    .toContainHTML('<h2>Google</h2>')
+
+  await fireEvent.click(getByText('Hide'))
+
+  expect(container)
+    .not.toContainHTML('<h2>Google</h2>')
 });
